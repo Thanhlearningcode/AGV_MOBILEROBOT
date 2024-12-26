0aecd55985838292b2c00b332eaf4df48535826c
Thanh Nguyen Van <145493670+Thanhlearningcode@users.noreply.github.com>
2024-12-26 11:05:07 +0700

    [PBL4/AGV]-Update mainpp.cpp

--- Source/STM32/mainpp.cpp
+++ Source/STM32/mainpp.cpp
@@ -132,7 +132,7 @@ void USART2_IRQHandler(void) {
 			Direction_left = (b1 == 0) ? 1: 0 ;
 			
 		pos_left = (Direction_left == 1) ?  ((pos_left == encoder_maximum) ? encoder_minimum : ++pos_left) :
-																			 	((pos_left == encoder_minimum) ? encoder_maximum : --pos_left) ;
+						    ((pos_left == encoder_minimum) ? encoder_maximum : --pos_left) ;
 			} 
 	}
 void Encoder_Turnright(void) {	
@@ -141,7 +141,7 @@ void Encoder_Turnright(void) {
 			Direction_right = ( b2==0 ) ? 1 : 0 ;
 		
 		pos_right = (Direction_right ==1) ? ((pos_right == encoder_maximum) ? encoder_minimum : ++pos_right) :
-																				((pos_left  == encoder_minimum) ? encoder_maximum : --pos_right) ;
+						    ((pos_left  == encoder_minimum) ? encoder_maximum : --pos_right) ;
 			} 
 	}
 /************************************* Motor Controller Functions ******************************/
